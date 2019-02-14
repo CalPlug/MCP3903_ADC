f29fb543502301f7cb5314ac5045a44181b5c91f
Michael John Klopfer <mklopfer@uci.edu>
2019-02-14 14:51:16 -0800

    Update mcp3903example.ino

--- Library/Arduino version/MCP3903/mcp3903example/mcp3903example.ino
+++ Library/Arduino version/MCP3903/mcp3903example/mcp3903example.ino
@@ -16,22 +16,24 @@ University of California, Irvine
 
 
 MCP3903 mcp3903;  //Initialize object
-SPISettings settingsA(2000000, MSBFIRST, SPI_MODE0); //settings for the SPI for the MCP3903 (either 0,0 or 1,1, according to the datasheet are compatable modes)
+SPISettings settingsA(1000000, MSBFIRST, SPI_MODE0); //settings for the SPI for the MCP3903 (1MHZ, MSB in first, SPI Mode ) tested initially -- (either 0,0 (Arduino SPI mode 0) or 1,1 (Arduino SPI Mode 3) according to the datasheet are compatable modes)
 
 void setup()
 {
     SPI.begin(); //Start SPI
-	  mcp3903.init(11, 12, 13, 10); // Setings for your Arduino's SPI pins used for interfacing(pinMOSI, pinMISO, pinSPIClock, pinCS)
-    Serial.begin(9600); //printo tout on serial monitor at 9600bps
+	mcp3903.init(11, 12, 13, 10); // Setings for your Arduino's SPI pins used for interfacing(pinMOSI, pinMISO, pinSPIClock, pinCS)
+    Serial.begin(9600); //set serial comm at 9600 bps
     //Set your pins in the library
     SPI.beginTransaction(settingsA); //Start SPI transaction
-	  mcp3903.reset(MCP3903::OSR_256); 
+	mcp3903.reset(MCP3903::OSR_256); 
     mcp3903.setGain(1,MCP3903::GAIN_8);
-	  SPI.endTransaction(); //End SPI transaction	
+	SPI.endTransaction(); //End SPI transaction	
 }
 
-double ch1_gain = 2.36; //Your calibration gain for Ch1- (a default value of 2.36 is used in relation to the reference voltage, this may vary)
+//Set the gains and offsets for your ADC channels:
+double ch1_gain = 2.36; //Your calibration gain for Ch1 - (a default value of 2.36 is used in relation to the reference voltage, this may vary)
 double ch1_offset = 0; //Your calibration offset for Ch1
+
 
 void loop()
 {
